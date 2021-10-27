c2bba56c5b5351ab140b1d698d29623e33f8af3c
Darryl <darrylmangibin@gmail.com>
2021-10-27 14:10:48 +0800

    Database seeding

--- database/factories/ModelReviewFactory.php
+++ database/factories/ModelReviewFactory.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Model\Product;
 use Faker\Generator as Faker;
 
 $factory->define(App\Model\Review::class, function (Faker $faker) {
     return [
-        //
+        'product_id' => function () {
+            return Product::all()->random()->id;
+        },
+        'customer' => $faker->name(),
+        'review' => $faker->paragraph(),
+        'star' => $faker->numberBetween(0, 5)
     ];
 });
