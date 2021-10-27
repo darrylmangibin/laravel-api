89e4e4ea2fa950531abac2d5e5ccd2bd50b8521b
Darryl <darrylmangibin@gmail.com>
2021-10-27 14:16:19 +0800

    Create relationship

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -5,6 +5,8 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
-{
-    //
+{   
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
 }
